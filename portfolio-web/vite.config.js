647c0789cf1a70010ca3c666360f445e02b374d5
Yogesh815131 <ky815131@gmail.com>
2025-05-15 12:45:21 +0530

    chage icone and app title

--- portfolio-web/vite.config.js
+++ portfolio-web/vite.config.js
@@ -6,6 +6,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5050, // Set your desired port number here
+    open: true // Optional: automatically open browser
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
